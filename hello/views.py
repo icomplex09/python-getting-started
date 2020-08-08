63687e12e28857b13da008d1e66d1e30e5e9c6f5
icomplex09 <62794141+icomplex09@users.noreply.github.com>
2020-08-08 16:48:47 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 from .models import Greeting
 
@@ -17,3 +18,7 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+def api(request):
+    return JsonResponse({'foo': 'bar'})
+    
